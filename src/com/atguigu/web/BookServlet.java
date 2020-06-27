73da0204b0876f473db923076f885c6d57599d97
jf168-stack <1454381947@qq.com>
2020-06-27 22:33:45 +0800

    添加操作后，回到添加的图书所在的页面，修改，删除操作后回到操作页面

--- src/com/atguigu/web/BookServlet.java
+++ src/com/atguigu/web/BookServlet.java
@@ -21,12 +21,14 @@ public class BookServlet extends BaseServlet {
     }
 
     protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int pageNo = WebUtils.parseInt(req.getParameter("pageNo"), 0);
+        pageNo += 1;
         //获取提交的图书信息，封装成对象
         Book book = WebUtils.copyParamToBean(req.getParameterMap(), new Book());
         //调用servi|ce方法保存图书信息
         bookService.addBook(book);
         //请求重定向到图书列表
-        resp.sendRedirect(req.getContextPath()+"/manager/bookServlet?action=list");
+        resp.sendRedirect(req.getContextPath() + "/manager/bookServlet?action=page&pageNo=" + pageNo);
     }
 
     protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -35,7 +37,7 @@ public class BookServlet extends BaseServlet {
         //调用service的update方法更新数据
         bookService.updateBook(book);
         //请求重定向到图书列表
-        resp.sendRedirect(req.getContextPath()+"/manager/bookServlet?action=list");
+        resp.sendRedirect(req.getContextPath() + "/manager/bookServlet?action=page&pageNo=" + req.getParameter("pageNo"));
     }
 
     protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -44,7 +46,7 @@ public class BookServlet extends BaseServlet {
         //调用service方法删除该图书
         bookService.deleteBook(id);
         //请求重定向到图书列表
-        resp.sendRedirect(req.getContextPath()+"/manager/bookServlet?action=list");
+        resp.sendRedirect(req.getContextPath() + "/manager/bookServlet?action=page&pageNo=" + req.getParameter("pageNo"));
     }
 
     protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -61,7 +63,7 @@ public class BookServlet extends BaseServlet {
         int pageNo = WebUtils.parseInt(req.getParameter("pageNo"), 1);
         int pageSize = WebUtils.parseInt(req.getParameter("pageSize"), Page.PAGE_SIZE);
         //调用BookService.page(pageNo,pageSize)返回Page对象
-        Page<Book> page = bookService.page(pageNo,pageSize);
+        Page<Book> page = bookService.page(pageNo, pageSize);
         //保存Page对象到request域中
         req.setAttribute("page", page);
         //请求转发到、page/manager/book_manager.jsp
@@ -74,7 +76,7 @@ public class BookServlet extends BaseServlet {
         //根据图书编号查询对应图书
         Book book = bookService.queryBookById(id);
         //将图书信息放到request域中
-        req.setAttribute("book",book);
+        req.setAttribute("book", book);
         //请求转发到图书修改界面
         req.getRequestDispatcher("/pages/manager/book_edit.jsp").forward(req, resp);
     }
